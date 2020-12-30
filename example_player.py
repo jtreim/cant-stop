abc6737bdaf005b64985ec499ebe0fe095f46bb9
Jeff Reimschussel <20570258+jtreim@users.noreply.github.com>
2020-12-29 17:30:22 -0700

    Create example_player.py

---
+++ example_player.py
@@ -0,0 +1,20 @@
+import random
+from player import Player
+
+class DumbPlayer(Player):
+    def __init__(self, name):
+        self.pre_logic = 'I wanna be special, so here\'s an attribute I assign beforehand'
+        super(DumbPlayer, self).__init__(name)
+        self.post_logic = 'I wanna be extra special, so here\'s another attribute I assign later'
+
+    def decide_continuing_turn(self):
+        # A new function that the parent class doesn't have! I can use this internally
+        return random.randint(1, 2) == 1
+
+    def is_continuing_turn(self, board, changes):
+        # Decides to continue turn based on 50% chance the random int is a 1
+        return self.decide_continuing_turn()
+
+    def choose_move(self, moves, board, changes, invalid_move=False):
+        # Always selects the first available move
+        return moves[0]
